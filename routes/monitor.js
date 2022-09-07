832a8adcc8f3e8fa62152a45eac1803d05032528
Ngo Lam <ngolamaz3@gmail.com>
2022-09-07 23:52:42 +0700

    don't mention it

--- routes/monitor.js
+++ routes/monitor.js
@@ -6,11 +6,11 @@ module.exports = router.get("/monitor", async (req, res) => {
 
     let respond = { }
 
-    respond.osInfo = await sysInfo.osInfo()
+    //respond.osInfo = await sysInfo.osInfo()
     //respond.cpuCurrentSpeed = await sysInfo.cpuCurrentSpeed()
     //respond.memory = await sysInfo.mem()
-    respond.network = await sysInfo.networkStats()
-    //respond.processes = await sysInfo.processes()
+    //respond.network = await sysInfo.networkStats()
+    respond.processes = await sysInfo.processes()
 
 
     console.log(respond)
