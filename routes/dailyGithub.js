65d9576c935ab47d330a891d7b85f7b9c52e4ade
Ngo Lam <ngolamaz3@gmail.com>
2022-09-16 12:18:46 +0700

    Header change

--- routes/dailyGithub.js
+++ routes/dailyGithub.js
@@ -19,9 +19,9 @@ router.get("/daily", (req, res) => {
     })
 
     res.setHeader("Content-Type", "image/svg+xml")
-    //res.setHeader("Cache-Control", "no-cache")
-    res.setHeader("Cache-Control", "public, max-age=3600")
-    res.setHeader("Expires", new Date(Date.now() + 3600000).toUTCString())
+    res.setHeader("Cache-Control", "no-cache")
+    //res.setHeader("Cache-Control", "public, max-age=3600")
+    //res.setHeader("Expires", new Date(Date.now() + 3600000).toUTCString())
     res.send(`
    <svg
         width="500"
